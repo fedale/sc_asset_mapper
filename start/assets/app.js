38cb5ce93632d6a06583a42928c228220eb4309b
Danilo Di Moia <d.dimoia@waltertosto.it>
2024-02-16 15:05:10 +0100

    -

--- start/assets/app.js
+++ start/assets/app.js
@@ -2,9 +2,6 @@ import './bootstrap.js';
 import './styles/app.css';
 import alienGreeting from './lib/alien-greeting.js';
 
-
-
-
 alienGreeting('Give us all your candy!', false);
 console.log('Give us')
 console.log('This log comes from assets/app.jwwwws - welcome to AssetMapper! 🎉');
