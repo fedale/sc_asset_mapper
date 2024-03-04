80bee49b1673c492d2628457e51dadee2f4b0679
Danilo Di Moia <zitter@gmail.com>
2024-03-04 22:28:17 +0100

    -

--- start/src/Controller/VoyageController.php
+++ start/src/Controller/VoyageController.php
@@ -36,6 +36,15 @@ class VoyageController extends AbstractController
 
             $this->addFlash('success', 'Bon voyage!');
 
+            if ($request->headers->has('turbo-frame')) {
+                $stream = $this->renderBlockView('voyage/new.html.twig', 'stream_success',
+                [
+                    'voyage' => $voyage
+                ]);
+
+                $this->addFlash('stream', $stream);
+            }
+
             return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -64,7 +73,16 @@ class VoyageController extends AbstractController
 
             $this->addFlash('success', 'Voyage updated!');
 
-            return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
+            if ($request->headers->has('turbo-frame')) {
+                $stream = $this->renderBlockView('voyage/edit.html.twig', 'stream_success',
+                [
+                    'voyage' => $voyage
+                ]);
+
+                $this->addFlash('stream', $stream);
+            }
+
+            return $this->redirectToRoute('app_voyage_edit', ['id' => $voyage->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('voyage/edit.html.twig', [
@@ -77,10 +95,20 @@ class VoyageController extends AbstractController
     public function delete(Request $request, Voyage $voyage, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$voyage->getId(), $request->request->get('_token'))) {
+            $id = $voyage->getId();
             $entityManager->remove($voyage);
             $entityManager->flush();
 
             $this->addFlash('success', 'Voyage deleted!');
+
+            if ($request->headers->has('turbo-frame')) {
+                $stream = $this->renderBlockView('voyage/delete.html.twig', 'stream_success',
+                [
+                    'id' => $id
+                ]);
+
+                $this->addFlash('stream', $stream);
+            }
         }
 
         return $this->redirectToRoute('app_voyage_index', [], Response::HTTP_SEE_OTHER);
