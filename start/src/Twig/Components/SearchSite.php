c67526844e0ade4dfb50a39b5c4877c3661f49f5
Danilo Di Moia <zitter@gmail.com>
2024-03-17 16:03:04 +0100

    -

---
+++ start/src/Twig/Components/SearchSite.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\VoyageRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent()]
+class SearchSite
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function __construct(private VoyageRepository $voyageRepository)
+    {}
+
+    public function voyages(): array
+    {
+        if (!$this->query) {
+            return [];
+        }
+        return $this->voyageRepository->findBySearch($this->query, [], 10);
+    } 
+}
